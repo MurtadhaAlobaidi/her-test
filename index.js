af5a2e173a38e3bbec017ab8ebad5fae3c6da224
mhaao@kth.se <mhaao@kth.se>
2023-03-05 23:44:47 +0100

    ok

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send("Hello world! Murtadha Test");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
